4068b5779030a036af1159003e52179bdea654b0
Shivu <manjudarshana.kv@gmail.com>
2026-03-19 23:24:15 +0530

    demo7 executed after taking help from gpt

--- level1/demo7.c
+++ level1/demo7.c
@@ -14,25 +14,84 @@ void delete_scores(int **arr);
 
 
 #include<stdio.h>
+#include<stdlib.h>
+
+int* allocate_scores(int n);
+void read_scores(int *arr, int n);
+void display_scores(int *arr, int n);
+int calculate_total(int *arr, int n);
+void delete_scores(int **arr);
+
+int* allocate_scores(int n){
+    int *arr = (int*)malloc(n*sizeof(int));
+
+    if(arr == NULL){
+        printf("memory alocation failed\n");
+        return NULL;
+    }
+
+    return arr;
+}
+
+void read_scores(int *arr, int n){
+    int i;
+    printf("enter scores:\n");
+    for(i=0;i<n;i++){
+        printf("player %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+}
+
+void display_scores(int *arr, int n){
+    int i;
+    printf("scores: ");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int calculate_total(int *arr, int n){
+    int i,total=0;
+    for(i=0;i<n;i++){
+        total += arr[i];
+    }
+    return total;
+}
+
+void delete_scores(int **arr){
+    free(*arr);
+    *arr = NULL;
+}
 
 int main(){
-    int arr[100];
-    int n,i,total=0;
+    int n,total;
+    int *arr;
 
     printf("enter number of players: ");
     scanf("%d",&n);
 
-    printf("enter scores:\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    if(n <= 0){
+        printf("no players\n");
+        return 0;
     }
 
-    for(i=0;i<n;i++){
-        printf("%d ",arr[i]);
-        total = total + arr[i];
+    arr = allocate_scores(n);
+    if(arr == NULL){
+        return 0;
     }
 
-    printf("\ntotal is %d",total);
+    read_scores(arr,n);
+    display_scores(arr,n);
+
+    total = calculate_total(arr,n);
+    printf("total team score = %d\n",total);
+
+    delete_scores(&arr);
+
+    if(arr == NULL){
+        printf("memory freed succesfully\n");
+    }
 
     return 0;
 }
